565344ad03a59ee316e7a775fc559f4bcd8357b6
Hammad Ashraf <hammadkalmati581@gmail.com>
2026-09-07 02:57:11 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,172 @@
+# PYTHON LOOPS & CONDITIONAL STATEMENTS ASSIGNMENT #
+
+# QUESTION NO 01 :-
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# QUESTION NO 02 :-
+# for i in range(1, 21):
+#     if i % 2 != 0:
+#         print(i)
+
+
+# QUESTION NO 03 :-
+# for i in range(1, 11):
+#     if i == 5:
+#         print("Five Found")
+#     else:
+#         print(i)
+
+
+# QUESTION NO 04 :-
+# data = [3, 8, 12, 1, 9]
+# for i in data:
+#     if i > 5:
+#         print(f"{i} is greater than 5")
+#     else:
+#         print(i)
+
+
+# QUESTION NO 05 :-
+# for i in range(1, 16):
+#     if i % 3 == 0:
+#         print(i)
+
+
+# QUESTION NO 06 :-
+# numbersList = [2, -5, 10, -8, 0]
+# for i in numbersList:
+#     if i > 0:
+#         print('Positive')
+#     elif i < 0:
+#         print('Negative')
+#     else:
+#         print('Zero')
+
+
+# QUESTION NO 07 :-
+# for i in range(1, 11):
+#     print(i)
+
+#     if i == 7:
+#         break
+
+
+# QUESTION NO 08 :-
+# for i in range(1 , 11):
+#     if i == 4:
+#         continue
+        
+#     print(i)
+
+
+# QUESTION NO 09 :-
+# word = 'Python'
+
+# for i in word:
+#     if i in 'aeiou':
+#         print(i)
+    
+
+# QUESTION NO 10 :-
+# ages = [12, 18, 25, 15]
+
+# for i in ages:
+#     if i >= 18:
+#         print('Adult')
+#     else:
+#         print('Minor')
+
+
+# QUESTION NO 11 :-
+# names = ['Ali', 'Sara', 'Zain']
+
+# for i in names:
+#     if i in 'Sara':
+#         print(f"{i} is present !")
+
+
+# QUESTION NO 12 :-
+# for i in range(1, 21):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print('FizzBuzz')
+
+
+# QUESTION NO 13 :-
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(i * 2)
+#     else:
+#         print(i)
+
+
+# QUESTION NO 14 :-
+# stdNames = ['Ali', 'Bilal', 'Ahmed', 'Sana']
+
+# for i in stdNames:
+#     if i.startswith('A'):
+#         print(f"Hello {i}")
+
+
+# QUESTION NO 15 :-
+# fruits = ['apple', 'mango', 'apple', 'banana', 'apple']
+# count = 0
+
+# for i in fruits:
+#     if i == 'apple':
+#         count = count + 1
+
+# print(count)
+
+
+# QUESTION NO 16 :-
+# marks = [45, 80, 30, 95, 40]
+
+# for i in marks:
+#     if i > 50:
+#         print('Pass')
+#     else:
+#         print('Fail')
+
+
+# QUESTION NO 17 :-
+# numbers = [150, 45, 200, 99, 120]
+
+# for i in numbers:
+#     if i > 100 and i < 200:
+#         print(i)
+
+
+# QUESTION NO 18 :-
+# for i in range(10, 0, -1):
+#     if i == 5:
+#         print('Halfway there..')
+#     else:
+#         print(i)
+
+
+# QUESTION NO 19 :-
+# total = 0
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         total = total + i
+
+# print(total)
+
+
+# QUESTION NO 20 :-
+# password = ['123', 'abc', 'admin123', 'xyz']
+
+# for i in password:
+#     if i == 'admin123':
+#         print('Access Granted')
+#         break
+
+
+
+
+
+
